dbb61609bc6dcd7872c0b11c25468060466f9e40
donpelo <mrgelladuga@gmail.com>
2025-12-14 00:23:19 -0300

    Añadidos módulos core: lector, audio y devocionales

---
+++ core/audio.py
@@ -0,0 +1,7 @@
+﻿import pyttsx3
+class AudioReader:
+    def __init__(self):
+        self.engine = pyttsx3.init()
+    def speak(self, text):
+        self.engine.say(text)
+        self.engine.runAndWait()
